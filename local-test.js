f994a7a51fc99ed37aeeaa14faa8781644230f30
ben7th <ben7th@sina.com>
2018-09-19 03:54:47 +0800

    update local-test

--- local-test.js
+++ local-test.js
@@ -1,12 +1,41 @@
 const icpAPI = require('./index')
 
-const test = () => {
-  console.log('发起请求')
-  
-  icpAPI.getMonthEI({ period: '2018_08_15' })
-    .then(data => {
-      console.log(data)
-    })
+const getYearEI = async ({ year }) => {
+  let periods = [...Array(12).keys()].map(x => {
+    let m = x + 1
+    m = m >= 10 ? `${m}` : `0${m}`
+    return `${year}_${m}_15`
+  })
+
+  let re = {}
+  for (let period of periods) {
+    let data = await icpAPI.getMonthEI({ period })
+    re[period] = data
+  }
+
+  return re
+}
+
+const getMonthsEI = async ({ year, fromMonth, toMonth }) => {
+  let periods = []
+  for (let m = fromMonth; m <= toMonth; m++) {
+    let m1 = m >= 10 ? `${m}` : `0${m}`
+    periods.push(`${year}_${m1}_15`)
+  }
+
+  let re = {}
+  for (let period of periods) {
+    let data = await icpAPI.getMonthEI({ period })
+    re[period] = data
+  }
+
+  return re
+}
+
+const test = async () => {  
+  // let data = await getYearEI({ year: 2018 })
+  let data = await getMonthsEI({ year: 2018, fromMonth: 5, toMonth: 8})
+  console.log(data)
 }
 
 test()
